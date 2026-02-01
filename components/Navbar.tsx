8ffe36f0845d95160aa7ef63a3802368783ec5c9
HavilahUAC <havilahuac@gmail.com>
2026-02-01 14:44:31 +0100

    Hello World

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return (
+        <nav className="w-full">
+            <div className="flex items-center justify-center h-[100px] gap-5 w-[60%] bg-gray-300 rounded-[30px]">
+                <div>
+                    <h1 className="font-mono">
+                        Hello World
+                    </h1>
+                </div>
+                <div></div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
